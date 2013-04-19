a08f2ac7ec9a1e9962fb3a5f52d787db383288ee
Graham MacDonald <grahamamacdonald@gmail.com>
2013-04-19 17:38:59 +0100

    More emacs

--- init.el
+++ init.el
@@ -7,6 +7,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; General
 
+; TODO Check we're at work!
+(when *is-windows*
+  (setq url-proxy-services '(("http" . "proxy.trayport.com:80"))))
+
 ; http://stackoverflow.com/questions/145175/how-to-invoke-an-interactive-elisp-interpreter-in-emacs
 ; ???
 ; (setq debug-on-error t)
@@ -252,3 +256,30 @@ by using nxml's indentation rules."
    (interactive)
    (find-file (concat org-directory "bugs.org")))
 
+
+; http://metajack.im/2009/01/01/journaling-with-emacs-orgmode/
+(defvar org-journal-file (concat org-directory "journal.org")
+  "Path to OrgMode journal file.")
+(defvar org-journal-date-format "%Y-%m-%d"
+  "Date format string for journal headings.")
+
+(defun org-journal-entry ()
+  "Create a new diary entry for today or append to an existing one."
+  (interactive)
+  (switch-to-buffer (find-file org-journal-file))
+  (widen)
+  (let ((isearch-forward t) (today (format-time-string org-journal-date-format)))
+    (beginning-of-buffer)
+    (unless (org-goto-local-search-headings today nil t)
+      ((lambda () 
+         (org-insert-heading)
+         (insert today)
+         (insert "\n\n  \n"))))
+    (beginning-of-buffer)
+    (org-show-entry)
+    (org-narrow-to-subtree)
+    (end-of-buffer)
+    (backward-char 2)
+    (unless (= (current-column) 2)
+      (insert "\n\n  "))))
+(global-set-key "\C-cj" 'org-journal-entry)
