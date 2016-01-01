23503292139561bb68b52aaba17341f850981d8f
Graham MacDonald <grahamamacdonald@gmail.com>
2016-01-01 08:34:46 +0000

    git fmt on save

--- init.el
+++ init.el
@@ -86,6 +86,22 @@
 ;; Open .h files in C++ mode
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Go
+
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Linux
 
